075b152387bc071d3f0d8c8162f200e95aef92ab
mi-dy <mindaugasdy@gmail.com>
2025-07-19 15:54:42 +0300

    Updated draw method

--- board.py
+++ board.py
@@ -1,13 +1,18 @@
 import sys
 
 class Board:
-    def __init__(self, ln1=["", "", ""], ln2=["", "", ""], ln3=["", "", ""]):
+    def __init__(self, ln1=[" ", " ", " "], ln2=[" ", " ", " "], ln3=[" ", " ", " "]):
         self.ln1 = ln1
         self.ln2 = ln2
         self.ln3 = ln3
 
     def draw(self):
-        print(f"{self.sq1}|{self.sq2}|{self.sq3}\n-----\n{self.sq4}|{self.sq5}|{self.sq6}\n-----\n{self.sq7}|{self.sq8}|{self.sq9}")
+        i = 0
+        for line in [self.ln1, self.ln2, self.ln3]:
+            print("|".join(item for item in line))
+            if i <= 1:
+                print("------")
+                i += 1
 
     def place_x(self, position):
         setattr(self, position, "x")
